e1e9d6dc6afbe28c821b6c856589567d5a7b27d8
ROOT_MILLIMAN\Ted.Zakrzewski <ted.zakrzewski@milliman.com>
2022-06-01 14:46:27 -0500

    strings

--- Math/Calculator.cs
+++ Math/Calculator.cs
@@ -7,5 +7,14 @@ namespace CsharpPart1.Math
     public class Calculator
     {
         int num;
+
+        // strings
+        // are immutable, any manipulation or editing always results in a new string
+        // Place holders, joins for arrays
+        // \n new line
+        // \t tab
+        // \\ Backslash
+        // \' Single Quatation mark
+        // \" Double Quoatation mark
     }
 }
